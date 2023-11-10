edcf9b935f1e108793b37a759650681ab8caff0f
Vicente Sanchez <Vikom.sanchez@gmail.com>
2023-11-10 19:17:09 -0300

    feat: get user metrics

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -44,4 +44,8 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .filter((i) => i.userId === userId)
       .slice((page - 1) * 20, page * 20)
   }
+
+  async countByUserId(userId: string) {
+    return this.items.filter((i) => i.userId === userId).length
+  }
 }
